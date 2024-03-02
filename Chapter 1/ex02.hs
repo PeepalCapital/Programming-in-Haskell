c8b8ba910d25c012618e5e75883f4c088c790cfc
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-03-01 19:51:06 -0800

    Update ex02.hs

--- Chapter 1/ex02.hs
+++ Chapter 1/ex02.hs
@@ -5,6 +5,7 @@ sum [x]
 applying sum
 x + sum []
 x + 0
+x
 
 Probably this is like operations in abstract algebra. For instance in a set (in this case a list) of
 numbers where the operation is a + there will be an inverse element and an identity element. In the 
