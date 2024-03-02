294b48f8c62f35e84adee69064cc358d3c08a07f
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-03-01 19:50:49 -0800

    Add files via upload

---
+++ Chapter 1/ex02.hs
@@ -0,0 +1,17 @@
+
+{- 
+
+sum [x]
+applying sum
+x + sum []
+x + 0
+
+Probably this is like operations in abstract algebra. For instance in a set (in this case a list) of
+numbers where the operation is a + there will be an inverse element and an identity element. In the 
+operation + the inverse is minus of a number in the set and identity is 0 such that adding 0 to that 
+number doesn't change anything.
+
+
+
+
+-}
