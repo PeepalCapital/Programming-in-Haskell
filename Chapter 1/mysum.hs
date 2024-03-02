e2d1472248fb2bd58371661e824fbf1063722b78
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-03-01 19:30:09 -0800

    Update mysum.hs

--- Chapter 1/mysum.hs
+++ Chapter 1/mysum.hs
@@ -2,6 +2,8 @@
 -- an ambiguous reference to sum. It should be Prelude.sum or Main.sum
 -- I don't know the reasons yet
 
+-- Update: Silly me. Sum is a predefined function in Haskell!
+
 mysum :: Num a => [a] -> a
 mysum [] = 0
 mysum (n:ns) = n + mysum ns
